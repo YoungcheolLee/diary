2b89c9397e14fbb25a4e98f2f78cd58c5ff1e994
Youngcheol <zodiac0905@gmail.com>
2022-09-27 21:24:42 +0900

    [feat] API를 이용하여 일기 데이터 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import Lifecycle from "./Lifecycle";
 
+// API 호출용 주소
+// https://jsonplaceholder.typicode.com/comments
+
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  // 컴포넌트가 마운트 되는 시점에 API를 호출하는 메서드
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
